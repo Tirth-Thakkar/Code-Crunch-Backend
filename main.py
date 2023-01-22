da48e6e04bda125bc2c0f006b538bc4b77f3a389
Tirth-Thakkar <tirth1226@gmail.com>
2023-01-21 22:33:02 -0800

    better

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import threading
 from flask import render_template  # import render_template from "public" flask libraries
 
 # import "packages" from "this" project
-from __init__ import app  # Definitions initialization
+from init import app  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+from api.user_api import user_api # Blueprint import api definition
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
+from api.user_api import user_api # Blueprint import api definition
+from model.users import initUsers
 
 # register URIs
 app.register_blueprint(joke_api) # register api routes
@@ -41,7 +43,7 @@ def activate_job():
     initUsers()
 
 # this runs the application on the development server
-if __name__ == "__main__":
+if name == "main":
     # change name for testing
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
     app.run(debug=True, host="0.0.0.0", port="8086")
