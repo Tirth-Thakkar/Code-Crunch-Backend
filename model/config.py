d92d4343d43a42fd773a6e40ccc5140fe09ad6e8
Ekamjot-Kaire <ekamjot.kaurr@gmail.com>
2023-01-22 22:30:28 -0800

    removing connexion module

--- model/config.py
+++ model/config.py
@@ -1,10 +1,10 @@
 import pathlib
-import connexion 
+#import connexion 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow #
 
 basedir = pathlib.Path(__file__).parent.resolve()
-connex_app = connexion.App(__name__, specification_dir=basedir)
+#connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'user_accounts.db'}"
