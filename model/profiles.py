b1230003c2132f56c535ca2c6e6a72ed77c90c8a
DeDuva4 <dhruvasiyer@gmail.com>
2023-02-13 11:49:56 -0800

    profile sample data

--- model/profiles.py
+++ model/profiles.py
@@ -8,14 +8,14 @@ class Profile(db.Model):
     __tablename__ = 'profiles'
     _username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     _email = db.Column(db.String(120), unique=True, nullable=False)
-    _high_score = db.Column(db.Integer(500), unique=False, nullable=False)
+    _high_high_score = db.Column(db.Integer(500), unique=False, nullable=False)
     _starred_games = db.Column(db.Integer(100), unique=True, nullable=False)
     _points_per_second = db.Column(db.Integer(100), unique=False, nullable=False)
 
-    def __init__(self, username, email, high_score, starred_games, points_per_second):
+    def __init__(self, username, email, high_high_score, starred_games, points_per_second):
         self._username = username
         self._email = email
-        self._high_score = high_score
+        self._high_high_score = high_high_score
         self._starred_games = starred_games
         self._points_per_second = points_per_second
 
@@ -40,15 +40,15 @@ class Profile(db.Model):
     def email(self, email):
         self._email = email
 
-    # high score getter and setter functions
+    # high high_score getter and setter functions
     @property
-    def high_score(self):
-        return self._high_score
+    def high_high_score(self):
+        return self._high_high_score
 
-    @high_score.setter
-    def high_score(self, high_score):
-        if high_score > self._high_score:
-            self._high_score = high_score
+    @high_high_score.setter
+    def high_high_score(self, high_high_score):
+        if high_high_score > self._high_high_score:
+            self._high_high_score = high_high_score
 
     # starred games getter and setter functions
     # not yet sure how this data will be stored, but I'm creating the getter and setter of some string called _starred_games now
@@ -85,13 +85,14 @@ class Profile(db.Model):
             return None
 
     def read(self):
-        return {'username': self._username, 'email': self._email, 'high_score': self._high_score, 'points_per_second': self._points_per_second}
+        return {'username': self._username, 'email': self._email, 'high_high_score': self._high_high_score, 'points_per_second': self._points_per_second}
 
-    def update(self, username, email, high_score, points_per_second):
+    def update(self, username, email, high_high_score, starred_games, points_per_second):
         self._username = username
         self._email = email
-        if high_score > self._high_score:
-            self._high_score = high_score
+        if high_high_score > self._high_high_score:
+            self._high_high_score = high_high_score
+        self._starred_games = starred_games
         self._points_per_second = points_per_second
         return self
 
@@ -99,3 +100,33 @@ class Profile(db.Model):
         db.session.delete(self)
         db.session.commit()
         return None
+    
+    def initProfile(self):
+        with app.app_context():
+            db.create_all()
+            db.session.commit()
+            db.session.remove()
+            return None
+        
+def initProfile():
+    with app.app_context():
+        """Create database and tables"""
+        db.init_app(app)
+        db.create_all()
+        """Tester data for table"""
+        profile1 = profile(username='sreeja', email = "sreeja@gmail.com", high_score=5, starred_games = 5, points_per_second = 3)
+        profile2 = profile(username='ekam', email = "ekam@gmail.com", high_score=4, starred_games = 4, points_per_second = 2)
+        profile3 = profile(username='tirth', email = "tirth@gmail.com", high_score=3, starred_games = 12, points_per_second = 1)
+        profile4 = profile(username='mani', email = "mani@gmail.com", high_score=2, starred_games = 6, points_per_second = 2)
+        profile5 = profile(username='dhruva', email = "dhruva@gmail.com", high_score=6, starred_games = 3, points_per_second = 3)
+        profiles = [profile1, profile2, profile3, profile4, profile5]
+
+
+        """Builds sample user/note(s) data"""
+        for profile in profiles:
+            try:
+                profile.create()
+            except IntegrityError:
+                '''fails with bad or duplicate data'''
+                db.session.remove()
+                print(f"Records exist, duplicate email, or error: {profile.username}")
