e240faf568fc8c37c03bac32e05e5ef7d8f89f9c
Tirth-Thakkar <tirth1226@gmail.com>
2023-02-14 19:42:57 -0800

    fixed some issues with garbage from dhruva

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.leader import leader_api # Blueprint import api definition
+from api.profile import profile_api
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
@@ -23,6 +24,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(leader_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(profile_api) # register api routes
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
