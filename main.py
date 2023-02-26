45863f559129b774a483f671ee838d69824fcda0
Tirth-Thakkar <tirth1226@gmail.com>
2023-02-25 20:27:34 -0800

    had to remove dhruva routing

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from __init__ import app, db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.leaders import initLeaders
-from model.profiles import initProfiles
+# from model.profiles import initProfiles
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.leader import leader_api # Blueprint import api definition
-from api.profile import profile_api # Blueprint import api definition
+# from api.profile import profile_api # Blueprint import api definition
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
@@ -24,7 +24,7 @@ app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(leader_api) # register api routes
-app.register_blueprint(profile_api) # register api routes
+# app.register_blueprint(profile_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -46,7 +46,7 @@ def activate_job():
     initJokes()
     initUsers()
     initLeaders()
-    initProfiles()
+    # initProfiles()
 
 # this runs the application on the development server
 if __name__ == "__main__":
